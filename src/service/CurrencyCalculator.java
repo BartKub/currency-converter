0c71139d6b0b02c40cc9b492c6630670ebc3171e
Bartlomiej Kubica <bartlomiej.kubica89@gmail.com>
2016-12-11 16:10:47 +0100

    Final Version

--- src/service/CurrencyCalculator.java
+++ src/service/CurrencyCalculator.java
@@ -1,43 +1,32 @@
 package service;
 
+import java.math.BigDecimal;
+
+import model.Currency;
+
 public class CurrencyCalculator {
-	private String base;
-	private String date;
-	private String rates;
+	
+	private CurrencyRatesDownloader ratesDownloader;
 
-	public CurrencyCalculator() {
+	//konstruktor parametryczny
+	//do zbudowania
+	
+	public CurrencyCalculator(CurrencyRatesDownloader ratesDownloader) {
+	
+		this.ratesDownloader = ratesDownloader;
+	}
+
+	public BigDecimal calculate(BigDecimal amount, Currency from, Currency to) {
 		
+		BigDecimal rate = ratesDownloader.downloadRate(from, to);
+	
+		if (rate == null){
+			return BigDecimal.ZERO;
+		}
+		
+		
+		return amount.multiply(rate);
 	}
 
-	public CurrencyCalculator(String base, String date, String rates) {
-
-		this.base = base;
-		this.date = date;
-		this.rates = rates;
-	}
-
-	public String getBase() {
-		return base;
-	}
-
-	public void setBase(String base) {
-		this.base = base;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getRates() {
-		return rates;
-	}
-
-	public void setRates(String rates) {
-		this.rates = rates;
-	}
 
 }
