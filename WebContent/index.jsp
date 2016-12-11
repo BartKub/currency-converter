5626842d1476da7253efc8ce0ae62b346bc0f87e
Bartlomiej Kubica <bartlomiej.kubica89@gmail.com>
2016-12-11 12:34:21 +0100

    Currency Test

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -80,7 +80,6 @@
 
         </form>
 
-        
 
         <div class="margin-top">
 
--- src/service/CurrencyCalculator.java
+++ src/service/CurrencyCalculator.java
@@ -1,5 +1,43 @@
 package service;
 
 public class CurrencyCalculator {
+	private String base;
+	private String date;
+	private String rates;
+
+	public CurrencyCalculator() {
+		
+	}
+
+	public CurrencyCalculator(String base, String date, String rates) {
+
+		this.base = base;
+		this.date = date;
+		this.rates = rates;
+	}
+
+	public String getBase() {
+		return base;
+	}
+
+	public void setBase(String base) {
+		this.base = base;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getRates() {
+		return rates;
+	}
+
+	public void setRates(String rates) {
+		this.rates = rates;
+	}
 
 }
